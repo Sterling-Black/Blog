e2a550450643729361f78600183a3bf331095c1b
sterling-black <tambongsterling@gmail.com>
2023-01-22 14:15:55 +0100

    Update the PORTS

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const _ = require("lodash");
 
 mongoose.set('strictQuery', false);
 
-console.log(process.env.MONGODB_URL);
-
 mongoose.connect(process.env.MONGODB_URL,{useNewUrlParser: true});
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -101,6 +99,6 @@ app.get("/post/:id", function(req,res){
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
